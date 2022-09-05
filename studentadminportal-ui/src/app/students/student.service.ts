f03bc86b5b8899984ba82f17d2354103c798edf6
Erick Aulestia <pxncxke@gmail.com>
2022-09-04 23:42:44 -0500

    table api configs

---
+++ studentadminportal-ui/src/app/students/student.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Student } from '../models/api-models/student.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StudentService {
+private baseApiUrl = 'https://localhost:7070';
+  constructor(private httpClient : HttpClient) { }
+
+  getStudent():Observable<Student[]>{
+    return this.httpClient.get<Student[]>(this.baseApiUrl + '/students');
+  }
+}
