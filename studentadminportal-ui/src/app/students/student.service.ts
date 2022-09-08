4702ebe093d169c606e6540e643313729e930456
Erick Aulestia <pxncxke@gmail.com>
2022-09-08 18:17:57 -0500

    view student, update

--- studentadminportal-ui/src/app/students/student.service.ts
+++ studentadminportal-ui/src/app/students/student.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Student } from '../models/api-models/student.model';
+import { UpdateStudentRequest } from '../models/api-models/update-student-request.model';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,28 @@ export class StudentService {
 private baseApiUrl = 'https://localhost:7070';
   constructor(private httpClient : HttpClient) { }
 
-  getStudent():Observable<Student[]>{
+  getStudents():Observable<Student[]>{
     return this.httpClient.get<Student[]>(this.baseApiUrl + '/students');
+  }
+
+  getStudent(studentId: string):Observable<Student>{
+    return this.httpClient.get<Student>(this.baseApiUrl + '/students/' + studentId);
+  }
+
+  updateStudent(studentId: string, studentRequest: Student): Observable<Student>{
+    const updateStudentRequest: UpdateStudentRequest = {
+      firstName: studentRequest.firstName,
+      lastName: studentRequest.lastName,
+      email: studentRequest.email,
+      mobile: studentRequest.mobile,
+      dateOfBirth: studentRequest.dateOfBirth,
+      genderId: studentRequest.genderId,
+      physicalAddress: studentRequest.address.physicalAddress,
+      postalAddress: studentRequest.address.postalAddress
+    }
+
+    return this.httpClient.put<Student>(this.baseApiUrl + "/students/" + studentId, updateStudentRequest);
+
+
   }
 }
---
+++ studentadminportal-ui/src/app/students/view-student/view-student.component.ts
@@ -0,0 +1,68 @@
+import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute } from '@angular/router';
+import { Gender } from 'src/app/models/ui-models/gender.model';
+import { Student } from 'src/app/models/ui-models/student.model';
+import { GenderService } from 'src/app/services/gender.service';
+import { StudentService } from '../student.service';
+
+@Component({
+  selector: 'app-view-student',
+  templateUrl: './view-student.component.html',
+  styleUrls: ['./view-student.component.css']
+})
+export class ViewStudentComponent implements OnInit {
+
+  studentId: string | null | undefined;
+  student: Student = {
+    id: '',
+    firstName: '',
+    lastName: '',
+    dateOfBirth: '',
+    email: '',
+    mobile: 0,
+    genderId: '',
+    profileImageUrl: '',
+    gender: {
+      id: '',
+      description: ''
+    },
+    address: {
+      id: '',
+      physicalAddress: '',
+      postalAddress: ''
+    }
+  };
+
+  genderList: Gender[] = [];
+
+  constructor(private readonly studentService: StudentService,
+    private readonly route: ActivatedRoute, private readonly genderService: GenderService, private snackbar: MatSnackBar) { }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((params) => {
+      this.studentId = params.get('id');
+
+      if(this.studentId){
+        this.studentService.getStudent(this.studentId).subscribe((succesResponse)=>{
+          this.student = succesResponse;
+        });
+      }
+
+      this.genderService.getGenderList().subscribe((succesResponse)=>{
+        this.genderList = succesResponse;
+      });
+
+    })
+
+  }
+
+  onUpdate():void {
+    this.studentService.updateStudent(this.student.id, this.student)
+    .subscribe((succesResponse)=>{
+      this.snackbar.open("Student updated successfully", undefined, {duration: 200});
+    }
+    )
+  }
+
+}
